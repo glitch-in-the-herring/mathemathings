9598d90d074f2284f53ec0b08099d1c68a64593d
Red Herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-04-18 13:30:12 +0700

    Create vigenere.py

---
+++ vigenere.py
@@ -0,0 +1,20 @@
+import math
+
+plaintext, key = str(), str()
+while not plaintext.isalpha():
+	plaintext = input("Plaintext... ").upper()
+
+while not key.isalpha():	
+	key = input("Key... ").upper()
+
+if len(key) < len(plaintext):
+	key = (key * math.ceil(len(plaintext)/len(key)))[0:(len(key) * math.ceil(len(plaintext)/len(key))) - (len(key) * math.ceil(len(plaintext)/len(key)) % len(plaintext))]
+elif len(key) > len(plaintext):
+	key = key[0:len(plaintext)]
+
+plaintext_num = [ord(x) - 64 for x in plaintext]
+key_num = [ord(x) - 64 for x in key]
+
+ciphertext = ''.join([chr(((plaintext_num[i] + key_num[i] - 1) % 26) + 64) for i in range(len(plaintext))])
+
+print(f"Ciphertext: {ciphertext}")
